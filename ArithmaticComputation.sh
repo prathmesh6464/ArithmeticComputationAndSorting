fc731868b1a35a62830b3f4f4668513062262fe8
prathamesh <prathmesh6464@gmail.com>
2020-02-26 17:35:01 +0530

    MODIFIED : Added desending order feature.

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -2,6 +2,11 @@
 printf "Welcome\n"
 
 
+#VARIABLE
+declare -A storeResult
+declare -a storeDictionaryValue
+
+
 #TAKING THREE INPUTS FROM USER  
 read -p "Enter First Input a : " a
 read -p "Enter Second Input b : " b
@@ -9,17 +14,17 @@ read -p "Enter Third Input c : " c
 
 
 #PERFORMING ARITHMETIC OPERATIONS
-echo "a+b*c : " $((a+b*c))
-echo "a*b+c : " $((a*b+c))
-echo "c/a+b : " `echo "scale=4; $c/$a+$b" | bc -l`
-echo "a%b+c : " `echo "scale=4; $a%$b+$c" | bc -l`
+echo "a+b*c : " `echo "scale=2; $a+$b*$c" | bc -l`
+echo "a*b+c : " `echo "scale=2; $a*$b+$c" | bc -l`
+echo "c/a+b : " `echo "scale=2; $c/$a+$b" | bc -l`
+echo "a%b+c : " `echo "scale=2; $a%$b+$c" | bc -l`
 
 
 #STORING RESULT IN DICTIONARY
-storeResult[0]=$((a+b*c))
-storeResult[1]=$((a*b+c))
-storeResult[2]=`echo "scale=4; $c/$a+$b" | bc -l`
-storeResult[3]=`echo "scale=4; $a%$b+$c" | bc -l`
+storeResult[0]=`echo "scale=2; $a+$b*$c" | bc -l`
+storeResult[1]=`echo "scale=2; $a*$b+$c" | bc -l`
+storeResult[2]=`echo "scale=2; $c/$a+$b" | bc -l`
+storeResult[3]=`echo "scale=2; $a%$b+$c" | bc -l`
 
 
 #DISPLAY DICTIONARY VALUES 
@@ -32,3 +37,26 @@ do
 	storeDictionaryValue[$iter]="${storeResult[$iter]}"
 	echo "${storeDictionaryValue[$iter]}"
 done
+
+
+#RESULT IN DECENDING ORDER
+for ((iter=0;iter<${#storeResult[@]}-1;iter++))
+do
+	for((iter2=$iter+1;iter2<${#storeResult[@]};iter2++))
+	do
+		if ((`echo "${storeDictionaryValue[$iter]}<${storeDictionaryValue[$iter2]}" | bc`==1 ))
+		then
+			temp="${storeDictionaryValue[$iter]}"
+			storeDictionaryValue[$iter]="${storeDictionaryValue[$iter2]}"
+			storeDictionaryValue[$iter2]=$temp
+		fi
+	done
+done
+
+
+#DISPLAY ARRAY VALUES
+echo ${storeDictionaryValue[@]}
+
+
+#RESULT IN ASENDING ORDER
+
