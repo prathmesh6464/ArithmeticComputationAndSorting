06dfd5d5537c9337942e75b6970e0a5a5de7c52e
prathamesh <prathmesh6464@gmail.com>
2020-02-26 16:12:07 +0530

    MODIFIED : Added feature result in dictionary.

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -13,3 +13,15 @@ echo "a+b*c : " $((a+b*c))
 echo "a*b+c : " $((a*b+c))
 echo "c/a+b : " `echo "scale=4; $c/$a+$b" | bc -l`
 echo "a%b+c : " `echo "scale=4; $a%$b+$c" | bc -l`
+
+
+#STORING RESULT IN DICTIONARY
+storeResult[0]=$((a+b*c))
+storeResult[1]=$((a*b+c))
+storeResult[2]=`echo "scale=4; $c/$a+$b" | bc -l`
+storeResult[3]=`echo "scale=4; $a%$b+$c" | bc -l`
+
+
+#DISPLAY DICTIONARY VALUES 
+echo "${storeResult[@]}"
+
