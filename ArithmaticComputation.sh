e598e93c8331a9b986a3db599fb7325b897d45aa
prathamesh <prathmesh6464@gmail.com>
2020-02-26 15:46:13 +0530

    MODIFIED : Added input features.

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -2,7 +2,7 @@
 printf "Welcome\n"
 
 
-#TAKING THREE INPUTS FROM USER
+#TAKING THREE INPUTS FROM USER  
 read -p "Enter First Input a : " a
 read -p "Enter Second Input b : " b
 read -p "Enter Third Input c : " c
